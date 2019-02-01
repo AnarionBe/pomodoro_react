d1eae259ca381bdb4b9e04e0e232a4b2aa472898
Marco De Bona - AnarionBe <marcodb.debona@gmail.com>
2019-02-01 09:21:49 +0100

    Updates

---
+++ precache-manifest.d446f36d45256b61b04bd133e58538a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6963ebc98c0d69cbad7a",
+    "url": "/AnarionBe/pomodoro_react/static/css/main.b2c10cc0.chunk.css"
+  },
+  {
+    "revision": "6963ebc98c0d69cbad7a",
+    "url": "/AnarionBe/pomodoro_react/static/js/main.6963ebc9.chunk.js"
+  },
+  {
+    "revision": "c844967cd4e108e7a733",
+    "url": "/AnarionBe/pomodoro_react/static/js/1.c844967c.chunk.js"
+  },
+  {
+    "revision": "18ad6b176aec68883736",
+    "url": "/AnarionBe/pomodoro_react/static/js/runtime~main.18ad6b17.js"
+  },
+  {
+    "revision": "3a653ca5a55c2dbb047f721a23e113ac",
+    "url": "/AnarionBe/pomodoro_react/static/media/logo.3a653ca5.png"
+  },
+  {
+    "revision": "22dd97f81488327c2302416100e1b600",
+    "url": "/AnarionBe/pomodoro_react/static/media/sound.22dd97f8.mp3"
+  },
+  {
+    "revision": "aed94e03ae7e7266d5c03775dd872b6e",
+    "url": "/AnarionBe/pomodoro_react/index.html"
+  }
+];
