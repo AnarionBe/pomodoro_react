fe49f1f54c212fe9af2d24b6f72b591192bc6a28
Marco De Bona - AnarionBe <marcodb.debona@gmail.com>
2019-02-01 09:44:56 +0100

    Updates

---
+++ precache-manifest.88b7b9d004e983112d5a2fbca3ade7aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6963ebc98c0d69cbad7a",
+    "url": "/pomodoro_react/static/css/main.b2c10cc0.chunk.css"
+  },
+  {
+    "revision": "6963ebc98c0d69cbad7a",
+    "url": "/pomodoro_react/static/js/main.6963ebc9.chunk.js"
+  },
+  {
+    "revision": "c844967cd4e108e7a733",
+    "url": "/pomodoro_react/static/js/1.c844967c.chunk.js"
+  },
+  {
+    "revision": "d58811638e95cc38d586",
+    "url": "/pomodoro_react/static/js/runtime~main.d5881163.js"
+  },
+  {
+    "revision": "3a653ca5a55c2dbb047f721a23e113ac",
+    "url": "/pomodoro_react/static/media/logo.3a653ca5.png"
+  },
+  {
+    "revision": "22dd97f81488327c2302416100e1b600",
+    "url": "/pomodoro_react/static/media/sound.22dd97f8.mp3"
+  },
+  {
+    "revision": "01db6c9f23b11903558f191166bf0e0a",
+    "url": "/pomodoro_react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/AnarionBe/pomodoro_react/precache-manifest.a3dfb2cefe0377c6960597f96697c8d0.js"
+  "/pomodoro_react/precache-manifest.88b7b9d004e983112d5a2fbca3ade7aa.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/AnarionBe/pomodoro_react/index.html", {
+workbox.routing.registerNavigationRoute("/pomodoro_react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
